dfd348035ff0abd118410edd259c43e5c9a96df1
Glitch (glitch-hello-node) <none>
2023-12-14 23:27:34 +0000

    📍👀 Checkpoint ./package.json:61502758/1246:61502763/1299 ./server.js:61502758/1535:61502763/2131 ./src/pages/index.hbs:61502758/3777 ./index.html:61502763/2107

--- server.js
+++ server.js
@@ -1,103 +1,30 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
-
 const path = require("path");
+var express = require('express');
+var app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+app.use(express.static('public'));
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false,
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
 });
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+// app.get("/main", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
+http.listen(3000, () => {
+  console.log("connected at 3000");
 });
 
-// Formbody lets us parse incoming forms
-fastify.register(require("@fastify/formbody"));
+io.on("connection", socket => {
+  console.log("a user connected!!");
 
-// View is a templating manager for fastify
-fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
+  socket.on("data", msg => {
+    io.emit("data", msg);
+  });
+
+  socket.on("disconnect", msg => {
+    console.log("user disconnected");
+  });
 });
-
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
-
-/**
- * Our home page route
- *
- * Returns src/pages/index.hbs with data built into it
- */
-fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-  
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-/**
- * Our POST route to handle and react to form submissions
- *
- * Accepts body data indicating the user choice
- */
-fastify.post("/", function (request, reply) {
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-
-    // Load our color data file
-    const colors = require("./src/colors.json");
-
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo,
-      };
-    } else {
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo,
-      };
-    }
-  }
-
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-// Run the server and report out to the logs
-fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Your app is listening on ${address}`);
-  }
-);
