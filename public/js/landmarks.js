60774b9acd6e241999ff03dcc6d5000c8665d7c8
Glitch (glitch-hello-node) <none>
2023-12-15 01:02:34 +0000

    🚁🍩 Checkpoint ./.glitch-assets:61502758/1141 ./public/js/landmarks.js:61502758/453 ./README.md:61502758/3532

--- public/js/landmarks.js
+++ public/js/landmarks.js
@@ -1,7 +1,7 @@
 const Landmarks = {
   "france": {
     previewUrl: "",
-    baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/eiffel.glb?v=1702600146260"
+    baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/eiffel.glb?v=1702601607283"
   },
   "italy": {
     previewUrl: "",
@@ -9,7 +9,7 @@ const Landmarks = {
   },
   "usa": {
     previewUrl: "",
-    baseUrl: ""
+    baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/sofliberty.glb?v=1702601787071"
   },
   "china": {
     previewUrl: "",
@@ -21,7 +21,7 @@ const Landmarks = {
   },
   "india": {
     previewUrl: "",
-    baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/tajmahal.glb?v=1702600156176"
+    baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/tajmahal.glb?v=1702601640246"
   },
   "australia": {
     previewUrl: "",
