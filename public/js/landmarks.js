b874ccef4e50c73345911119af08d4f021ee5f6f
Glitch (glitch-hello-node) <none>
2023-12-15 00:26:35 +0000

    🙏🌄 Checkpoint ./public/js/landmarks.js:61502763/2159

---
+++ public/js/landmarks.js
@@ -0,0 +1,38 @@
+const Landmarks = {
+  "france": {
+    previewUrl: "",
+    baseUrl: ""
+  },
+  "italy": {
+    previewUrl: "",
+    baseUrl: ""
+  },
+  "usa": {
+    previewUrl: "",
+    baseUrl: ""
+  },
+  "china": {
+    previewUrl: "",
+    baseUrl: ""
+  },
+  "peru": {
+    previewUrl: "",
+    baseUrl: ""
+  },
+  "india": {
+    previewUrl: "",
+    baseUrl: ""
+  },
+  "australia": {
+    previewUrl: "",
+    baseUrl: ""
+  },
+  "japan": {
+    previewUrl: "",
+    baseUrl: ""
+  },
+  "brazil": {
+    previewUrl: "",
+    baseUrl: ""
+  }
+}
