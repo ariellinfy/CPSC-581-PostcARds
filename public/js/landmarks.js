3c253c4efc579bf8e2f63cf6aeebba1f3390b60c
Glitch (alive-apricot-aluminum) <none>
2023-12-15 04:56:39 +0000

    👫🕵 Checkpoint ./src/pages/create.html:61502758/224 ./public/js/landmarks.js:61502758/1763

--- public/js/landmarks.js
+++ public/js/landmarks.js
@@ -1,52 +1,52 @@
 const Landmarks = {
   "france": {
-    previewUrl: "",
+    previewUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/eiffel.glb?v=1702601607283",
     baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/eiffel.glb?v=1702601607283",
-    scale: "1 1 1"
+    previewScale: "0.2 0.2 0.2"
   },
   "italy": {
-    previewUrl: "",
+    previewUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/colosseum.glb?v=1702602238137",
     baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/colosseum.glb?v=1702602238137",
-    scale: "1 1 1"
+    previewScale: "1 1 1"
   },
   "usa": {
-    previewUrl: "",
+    previewUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/sofliberty.glb?v=1702601787071",
     baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/sofliberty.glb?v=1702601787071",
-    scale: "1 1 1"
+    previewScale: "0.25 0.25 0.25"
   },
   "china": {
-    previewUrl: "",
+    previewUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/heaven_temple.glb?v=1702608328577",
     baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/heaven_temple.glb?v=1702608328577",
-    scale: "1 1 1"
+    previewScale: "0.2 0.2 0.2"
   },
   "mexico": {
-    previewUrl: "",
+    previewUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/mayan_pyramid.glb?v=1702610751767",
     baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/mayan_pyramid.glb?v=1702610751767",
-    scale: "1 1 1"
+    previewScale: "0.25 0.25 0.25"
   },
   "india": {
-    previewUrl: "",
+    previewUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/tajmahal.glb?v=1702601640246",
     baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/tajmahal.glb?v=1702601640246",
-    scale: "1 1 1"
+    previewScale: "1 1 1"
   },
   "australia": {
-    previewUrl: "",
+    previewUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/sydneyOHouse.glb?v=1702604713757",
     baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/sydneyOHouse.glb?v=1702604713757",
-    scale: "1 1 1"
+    previewScale: "1 1 1"
   },
   "greece": {
-    previewUrl: "",
+    previewUrl: "https://cdn.glitch.me/e76c002c-c9da-46f1-9730-df8080677b3f/nashville_parthenon.glb?v=1702610574563",
     baseUrl: "https://cdn.glitch.me/e76c002c-c9da-46f1-9730-df8080677b3f/nashville_parthenon.glb?v=1702610574563",
-    scale: "1 1 1"
+    previewScale: "1 1 1"
   },
   "japan": {
-    previewUrl: "",
+    previewUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/fushimi_inari_taisha.glb?v=1702605227729",
     baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/fushimi_inari_taisha.glb?v=1702605227729",
-    scale: "1 1 1"
+    previewScale: "1 1 1"
   },
   "brazil": {
-    previewUrl: "",
+    previewUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/christ_the_redeemer_cristo_redentor.glb?v=1702605683534",
     baseUrl: "https://cdn.glitch.global/e76c002c-c9da-46f1-9730-df8080677b3f/christ_the_redeemer_cristo_redentor.glb?v=1702605683534",
-    scale: "1 1 1"
+    previewScale: "1 1 1"
   }
 }
